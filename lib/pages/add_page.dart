bdcf50a87c1110fe131fe1f93d406a3dfc2abe37
reecerose <reecerose18@gmail.com>
2019-05-13 21:10:01 -0400

    Force close keyboard

--- lib/pages/add_page.dart
+++ lib/pages/add_page.dart
@@ -2,9 +2,18 @@ import 'package:Flutter_ObjectDB_Example/api/database_provider.dart';
 import 'package:flutter/material.dart';
 
 class AddPage extends StatelessWidget {
+  TextEditingController _nameController;
+  String _name = "";
+
+  AddPage() {
+    _nameController = TextEditingController(text: '');
+    _nameController.addListener(() {
+      _name = _nameController.text;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    String _name = "";
     return Container(
       padding: EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 0.0),
       child: Column(
@@ -15,22 +24,23 @@ class AddPage extends StatelessWidget {
               hintText: 'John Doe',
             ),
             keyboardType: TextInputType.text,
-            onChanged: (String value) {
-              _name = value;
-            },
+            controller: _nameController,
           ),
           FlatButton(
             color: Colors.blue,
             onPressed: () {
               if (_name.isEmpty) return;
-              // submit
               DatabaseProvider.db.insertName(_name);
-              _name = "";
+              // reset name
+              _nameController.text = '';
+              // Confirmation of insert
               Scaffold.of(context).showSnackBar(
                 SnackBar(
                   content: Text('Added name to local database'),
                 ),
               );
+              // Force close the keyboard
+              FocusScope.of(context).requestFocus(new FocusNode());
             },
             child: Text('Add'),
           )
